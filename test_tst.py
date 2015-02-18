5e6847a3930bd9bdd0dad2857897bfa105f8baa2
Darius Liddell <djtrack16@gmail.com>
2015-02-18 00:07:26 -0800

    tests pass

--- test_tst.py
+++ test_tst.py
@@ -2,22 +2,21 @@ from ternarysearchtree import TernarySearchTree
 import random
 import time
 
-test_prefixes = {
-	'boa': set([]),
-	'ano': set([]),
-	'lic': set([]),
-	'mon': set([]),
-	'fen': set([]),
-	'gir': set([]),
-	'pel': set([]),
-	'lio': set([]),
-	'que': set([]),
-	'coq': set([]),
-}
+prefixes = [
+	'boa',
+	'ano',
+	'mon',
+	'fen',
+	'gir',
+	'pel',
+	'lio',
+	'que',
+	'coq',
+	'lic'
+]
 
-def test_add_and_contains(words, prefixLen):
+def test_add_and_contains(words, prefixLen, test_prefixes):
 	t = TernarySearchTree()
-	#test_prefixes = {} # as we build the tree, we get all the words starting with the prefixes we want to test
 	maxInsert = 0
 	maxContains = 0
 	for w in words:
@@ -27,7 +26,6 @@ def test_add_and_contains(words, prefixLen):
 		t.insert(w)
 		add_end = time.time() - add_start
 		maxInsert = max(maxInsert, add_end)
-
 		if len(w) >= prefixLen:
 			if w[:prefixLen] in test_prefixes:
 				test_prefixes[w[:prefixLen]].add(w)
@@ -40,30 +38,25 @@ def test_add_and_contains(words, prefixLen):
 	print 'Max Insert time was %f sec, Max Membership Checking time was %f sec' % (maxInsert, maxContains)
 	return t
 
-def test_autocomplete(t, words):
+def test_autocomplete(t, test_prefixes):
 	# how do we know the words that the algorithm finds are all the possible words
-	#max_autocomplete_time = 0
 	for prefix in test_prefixes.keys():
 		# we can check the equality of sets this way (quite pythonic) or ask if symmetric difference is size 0
 		start = time.time()
-		completions = t.autocomplete(prefix)
-		assert(completions == test_prefixes[prefix])
+		completions = {word for word in t.autocomplete(prefix)}
+		#print completions
+		#print test_prefixes[prefix]
+		assert(completions - test_prefixes[prefix] == set())
 		print 'Autocomplete time for prefix "%s" with %d words was %f sec' % (prefix, len(completions), time.time() - start) 
 
-
-
-''' Generate an arbitrary prefix of length prefixLen and add it to dictionary '''
-def getPrefixes(prefixLen):
-	pass
 
 if __name__ == '__main__':
 	'''
 	Read in a large text file from path using 'cat /usr/share/dict/words > words.txt' (235K words) 
 	'''
 	f = open('./words.txt')
-	words = set([])
-	for line in f:
-		words.add(line[:-1])
-
-	t = test_add_and_contains(words, 3)
-	test_autocomplete(t, words)
+	words = {line[:-1] for line in f}
+	test_prefixes = {prefix: set([]) for prefix in prefixes}
+	t = test_add_and_contains(words, 3, test_prefixes)
+	test_autocomplete(t, test_prefixes)
+	
